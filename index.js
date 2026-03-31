faa053c78f5194f7f1af9c53a992601b8bb32143
Mohamad Hamsho <hamsho1030@gmail.com>
2026-03-30 20:24:24 -0400

    Added rendering to the page with the data

---
+++ index.js
@@ -0,0 +1,49 @@
+import { resources } from "./data.js";
+
+const renderedMovieEl = document.getElementById('rendered-movies')
+
+console.log(renderedMovieEl)
+
+console.log(window.location.href)
+console.log(document.body.innerHTML)
+
+
+function classInformation(){
+    const resource = resources
+    let resourceArray = []
+
+    for(let classAttribute of resource){
+        resourceArray.push(classAttribute)
+        }
+    return resourceArray
+    
+    }
+
+
+function renderClasses(){
+    const eachClass = classInformation()
+    let html = ""
+
+    for(let className of eachClass){
+        html += 
+    `<div class="classContainer" id="${className.id}">
+        <h2>${className.title}</h2>
+        <p>${className.category}</p>
+        <div>
+            <h6>Difficulty ${className.difficulty}</h6>
+            <h6>Time ${className.time}</h6>
+            <h6>Price ${className.price}</h6>
+        </div>
+        <div>
+        <h4>Description</h4>
+        <h5>${className.description}</h5>
+        </div>
+    </div>`
+
+    }
+    console.log(html)
+    renderedMovieEl.innerHTML = html
+}
+
+
+renderClasses()
